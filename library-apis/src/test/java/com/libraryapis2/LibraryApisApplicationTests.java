946820d2b7893e513597987a665fe5c54596687e
Senowa Mize-Fox <smizefox@Senowas-MacBook-Pro.local>
2020-05-09 14:16:00 -0400

    initial commit

---
+++ library-apis/src/test/java/com/libraryapis2/LibraryApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryapis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
